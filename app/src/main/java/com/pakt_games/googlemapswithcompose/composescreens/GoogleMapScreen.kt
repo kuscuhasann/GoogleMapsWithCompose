6400f2d0fbc7330d9fa97c843af41fd88559a4e6
Hasan KUŞÇU <52363291+kuscuhasann@users.noreply.github.com>
2023-01-02 20:51:21 +0300

    Update GoogleMapScreen.kt

--- app/src/main/java/com/pakt_games/googlemapswithcompose/composescreens/GoogleMapScreen.kt
+++ app/src/main/java/com/pakt_games/googlemapswithcompose/composescreens/GoogleMapScreen.kt
@@ -22,9 +22,7 @@ fun GoogleMapScreen(
     setupClusterManager: (Context, GoogleMap) -> ZoneClusterManager,
     calculateZoneViewCenter: () -> LatLngBounds,
 ) {
-    // Set properties using MapProperties which you can use to recompose the map
     val mapProperties = MapProperties(
-        // Only enable if user has accepted location permissions.
         isMyLocationEnabled = state.lastKnownLocation != null,
     )
     val cameraPositionState = rememberCameraPositionState()
@@ -61,37 +59,20 @@ fun GoogleMapScreen(
                 }
             }
 
-            // NOTE: Some features of the MarkerInfoWindow don't work currently. See docs:
-            // https://github.com/googlemaps/android-maps-compose#obtaining-access-to-the-raw-googlemap-experimental
-            // So you can use clusters as an alternative to markers.
             MarkerInfoWindow(
                 state = rememberMarkerState(position = LatLng(49.1, -122.5)),
                 snippet = "Some stuff",
                 onClick = {
-                    // This won't work :(
-                    System.out.println("Mitchs_: Cannot be clicked")
+                    System.out.println("Cannot be clicked")
                     true
                 },
                 draggable = true
             )
         }
     }
-//    // Center camera to include all the Zones.
-//    LaunchedEffect(state.clusterItems) {
-//        if (state.clusterItems.isNotEmpty()) {
-//            cameraPositionState.animate(
-//                update = CameraUpdateFactory.newLatLngBounds(
-//                    calculateZoneViewCenter(),
-//                    0
-//                ),
-//            )
-//        }
-//    }
+
 }
 
-/**
- * If you want to center on a specific location.
- */
 private suspend fun CameraPositionState.centerOnLocation(
     location: Location
 ) = animate(
